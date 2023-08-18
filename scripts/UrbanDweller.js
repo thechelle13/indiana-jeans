740a209325ad8a595b5427b122e18a9a3242f8ba
Michelle Totherow <tym4me13@gmail.com>
2023-08-18 14:04:40 -0500

    update to js from Loop to map method

--- scripts/UrbanDweller.js
+++ scripts/UrbanDweller.js
@@ -1,16 +1,11 @@
 import { setSocioLocationId } from "./TransientState.js"
 
-
 const handleLocationChange = (changeEvent) => {
     if (changeEvent.target.name === "location") {
         const convertedToInteger = parseInt(changeEvent.target.value)
         setSocioLocationId(convertedToInteger)
     }
 }
-
-
-
-
 
 export const LocationTypeChoices = async () => {
   
@@ -20,8 +15,13 @@ export const LocationTypeChoices = async () => {
     document.addEventListener("change", handleLocationChange)
 
     let choicesHTML = "<h2>Which type of area do you live in?</h2>"
-    for(const location of locations) {
-        choicesHTML += `<input type='radio' name='location' value='${location.id}'/> ${location.label}`
-    }
+    const divStringArray = locations.map( 
+        (location) => {
+            return `<input type='radio' name='location' value='${location.id}'/> ${location.label}`
+        }
+    )
+    choicesHTML += divStringArray.join("")
     return choicesHTML
+    
 }
+
